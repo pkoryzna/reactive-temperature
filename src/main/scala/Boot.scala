0a2acf65c5cf2328c53c4c12bef5fede886aa47d
Patryk Koryzna <pkoryzna@gmail.com>
2015-09-15 09:05:36 +0200

    add merging streams together (WIP)

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,44 +1,52 @@
+import java.io.File
+
 import akka.actor._
 import akka.stream.ActorMaterializer
 import akka.stream.actor.ActorPublisher
-import akka.stream.scaladsl.{Merge, Sink, Source}
-import temperature.FileRefresher
+import akka.stream.scaladsl._
+import sensor.SerialNumber
+import streaming.FileReloader
+
 import scala.concurrent.duration._
 
 object Boot extends App {
   implicit val system = ActorSystem("reactive-temperature")
-  implicit val materializer = ActorMaterializer()
+  implicit val mat = ActorMaterializer()
+
 
   def fileReloadSource(period: FiniteDuration, path: String): Source[String, Unit] = {
-    Source(ActorPublisher[String](system.actorOf(FileRefresher.props(period, path))))
+    Source(ActorPublisher[String](system.actorOf(FileReloader.props(period, path))))
   }
+
+
+  import akka.stream.scaladsl.FlowGraph
+  import system.log
+
+  val sensors = sensor.Sensor.find(new File("/Users/patryk/Src/temperature_sensor/src/main/resources/exampleData/"))
+
+
+
+
+  import FlowGraph.Implicits._
+
+
+
+  val g = FlowGraph.closed() { implicit b =>
+    val sensorSources =
+      sensors.map { sensor =>
+        fileReloadSource(5.seconds, sensor.device.getPath)
+          .filter(_.contains("t=")).map(_.split("\\s").last.replace("t=", "").toDouble)
+          .map((sensor.serialNumber, _))
+      }
+
+    val merge = b.add(Merge[(SerialNumber, Double)](sensorSources.size))
+    sensorSources.zipWithIndex.foreach { case (src, n) => b.addEdge(b.add(src), merge.in(n)) }
+
+    val consoleSink = b.add(Sink.foreach[(SerialNumber, Double)](st => log.info(s"sensor: ${st._1}, temp: ${st._2} C")))
+
+    merge.out ~> consoleSink
+  }
+
+  g.run()
   
-  val (period, path) = (10.seconds, "/exampleData/sensorData.txt")
-
-
-  val goodSensor = fileReloadSource(period, path)
-
-  val validTemperature =
-    goodSensor
-      .grouped(2)
-      .collect {
-        case Seq(crcLine, tempLine) if crcLine.contains("YES") => tempLine
-      }
-
-  val sensorReadouts =
-    validTemperature
-      .map { tempLine =>
-        val split = tempLine.split("\\s+")
-        val temp = split.last.replace("t=", "").toDouble
-        val celsius = temp / 1000
-
-        val serial = split.dropRight(1).mkString
-
-        (serial, celsius)
-      }
-
-  val sink = Sink.foreach[(String, Double)]( st => println(s"sensor: ${st._1}, temp: ${st._2} C") )
-
-  sensorReadouts.runWith(sink)
-
 }
